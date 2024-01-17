2ca6e095d5ed60957d367da49d12bf61443f7b77
Bobberlington <70496934+bobberlington@users.noreply.github.com>
2024-01-16 19:17:33 -0800

    Create credentials.py

---
+++ credentials.py
@@ -0,0 +1,8 @@
+# You may type in your linkedin credentials here. If they are not here, then the tool will prompt you to log in manually.
+email_login = None
+linkedin_password = None
+
+# A chatgpt key is REQUIRED for this tool to work.
+chatgpt_api = None
+
+
